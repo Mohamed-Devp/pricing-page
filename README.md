5a92f0f4e5eb68987e814d3c2ef8342b9569efbc
Mohamed <marghoubmohamed2@gmail.com>
2025-06-27 18:49:47 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# Pricing Page ✨
+
+This is a solution to the [Pricing component with toggle challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/pricing-component-with-toggle-8vPwRMIC). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [Preview](#preview)
+- [Author](#author)
+- [License](#license)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the component depending on their device's screen size
+- Control the toggle with both their mouse/trackpad and their keyboard
+- **Bonus**: Complete the challenge with just HTML and CSS
+
+### Preview
+
+![Preview screenshot](./images/preview.gif)
+
+### Links
+
+- Solution URL: [My solution](https://your-solution-url.com)
+- Live Site URL: [Live demo](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Media queries.
+
+## Author
+
+- Frontend Mentor - [@Mohamed-Devp](https://www.frontendmentor.io/profile/Mohamed-Devp)
+
+## License
+
+This project is licensed under the MIT License.
---
+++ index.js
@@ -0,0 +1,57 @@
+const toggle = document.querySelector('.toggle');
+const toggleButton = toggle.firstElementChild;
+
+const priceSpans = document.querySelectorAll('.price span');
+
+let activeType = 0; // 0: Monthly - 1: Yearly
+let prevSelected = 1;
+
+const monthlyPrices = [19.99, 24.99, 39.99];
+const yearlyPrices = [199.99, 249.99, 399.99];
+
+function toggleType() {
+    toggleButton.classList.toggle("active");
+    activeType = activeType == 0 ? 1 : 0;
+
+    // Update the prices
+    updatePrices();
+}
+
+function updatePrices() {
+    priceSpans.forEach((priceSpan, index) => {
+        priceSpan.classList.remove('active');
+        void priceSpan.offsetWidth;
+
+        priceSpan.innerHTML = activeType == 0
+            ? monthlyPrices[index] : yearlyPrices[index];
+
+        priceSpan.classList.add('active');
+    });
+}
+
+function onKeyDown(event) {
+    const { key } = event;
+
+    if (key != "ArrowLeft" && key != "ArrowRight") {
+        return;
+    }
+    
+    if (key == "ArrowLeft" && activeType != 1) {
+        activeType = 1;
+        toggleButton.classList.remove("active");
+
+    } else if (key == "ArrowRight" && activeType != 0) {
+        activeType = 0;
+        toggleButton.classList.add("active");
+    }
+
+    // Update the prices
+    updatePrices();
+}
+
+function onMount() {
+    toggle.addEventListener("click", toggleType);
+    document.addEventListener("keydown", onKeyDown);
+}
+
+document.addEventListener('DOMContentLoaded', onMount);
